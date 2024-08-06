d62c93db42977a3e2bb1752f9c716d444fdea255
thoma <thomashsu0512@gmail.com>
2024-08-06 15:14:48 -0400

    started js

---
+++ static/js/draw_functions.js
@@ -0,0 +1,63 @@
+/*
+    taken from https://codepen.io/julio_ok/pen/ozpqGO
+*/
+
+var square = document.getElementById("drawPlace");
+var paper = square.getContext("2d");
+var pressedMouse = false;
+var x;
+var y;
+var colorLine = "black";
+var timeout;
+
+square.addEventListener("mousedown", startDrawing);
+square.addEventListener("mousemove", drawLine);
+square.addEventListener("mouseup", stopDrawing);
+
+function startDrawing(eventvs01) {
+    pressedMouse = true;
+    x = eventvs01.offsetX;
+    y = eventvs01.offsetY;
+}
+
+function drawLine(eventvs02) {
+    if (pressedMouse) {
+        document.getElementById("drawPlace").style.cursor = "crosshair";
+        var xM = eventvs02.offsetX;
+        var yM = eventvs02.offsetY;
+        drawing_line(colorLine, x, y, xM, yM, paper);
+        x = xM;
+        y = yM;
+    }
+}
+
+function stopDrawing() {
+    pressedMouse = false;
+    document.getElementById("drawPlace").style.cursor = "default";
+
+    // Start a timeout to detect when the user has stopped drawing
+    clearTimeout(timeout);
+    timeout = setTimeout(generateImage, 1000);
+}
+
+function drawing_line(color, x_start, y_start, x_end, y_end, board) {
+    board.beginPath();
+    board.strokeStyle = color;
+    board.lineWidth = 5;
+    board.moveTo(x_start, y_start);
+    board.lineTo(x_end, y_end);
+    board.stroke();
+    board.closePath();
+}
+
+function clearCanvas() {
+    // Clear the entire canvas by setting the width and height to the same as the canvas element's width and height
+    paper.clearRect(0, 0, square.width, square.height);
+}
+
+function generateImage() {
+    var url = square.toDataURL('image/png');
+    clearCanvas();
+    appendExpression('6 ')
+    showExpression();
+}
--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,25 @@
-console.log("this is js")
+var expression = '';
+
+function appendExpression(value) {
+    expression += value;
+    showExpression();
+}
+
+function showExpression() {
+    document.getElementById("expression").innerHTML = expression;
+}
+
+function undo() {
+    expression = expression.slice(0, -2);
+    showExpression();
+}
+
+function clearExpression() {
+    expression = '';
+    showExpression();
+}
+
+function solve() {
+    expression += '5 ';
+    showExpression();
+}
